c7da9578a1e11306f016a622d37fc12fb6fb323e
okok111 <1for.rails@gmail.com>
2023-05-08 13:23:34 +0900

    crud

---
+++ todo-app/src/store/index.js
@@ -0,0 +1,38 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    count: 0,
+    memos:[]
+  },
+  getters: {
+    getCount:(state)=>{
+      return state.memos.length
+    },
+    getAll:(state)=>{
+      return state.memos
+    },
+    getMemoById:(state)=> (id) =>{
+      return state.memos.find(memo=>memo.id === id)
+    }
+  },
+  mutations: {
+    save(state, newMemo) {
+      if(newMemo.id){
+        let x = state.memos.find(memo=>memo.id === newMemo.id)
+        x.title= newMemo.title
+        x.content = newMemo.content
+      }else{
+        newMemo.id = ++state.count
+        state.memos.unshift(newMemo)
+      }
+    },
+    delete(state, id) {
+      state.memos = state.memos.filter(memo => memo.id !== id)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ todo-app/src/views/NewView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>new</h2>
+  <MemoForm memo=""/>
+</template>
+<script>
+  import MemoForm from  '@/components/MemoForm.vue'
+  export default {
+  name: 'NewView',
+  components:{
+      MemoForm
+  }
+}
+</script>
